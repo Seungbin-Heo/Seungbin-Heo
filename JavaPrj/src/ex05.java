84e4b2c1ef50b3a60b93115a41a865c657ab4141
DATA8320-14 <DATA8320-14@DESKTOP-D5BTGT0>
2020-05-14 15:30:09 +0900

    예제들 푼 거 올립니다.

---
+++ JavaPrj/src/ex05.java
@@ -0,0 +1,10 @@
+public class ex05 {
+
+	public static void main(String[] args) {
+		float a = 0.1234567890123456789012345f;
+		double b = 0.1234567890123456789012345;
+		
+		System.out.printf("%30.25f \n", a);
+		System.out.printf("%30.25f \n", b);
+	}
+}
